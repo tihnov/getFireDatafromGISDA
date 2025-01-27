80db7047f4376cdbb8c477e3b39f035d5bd3ae9f
tihnov <tihnov@gmail.com>
2025-01-27 12:01:45 +0000

    run on every 9 am

--- getDatafromGISDA.py
+++ getDatafromGISDA.py
@@ -4,6 +4,7 @@ import time
 from utility import log
 import os, sys
 import datetime
+from datetime import date, timedelta
 
 # show info
 print("base path : {}".format(os.path.dirname(sys.argv[0])))
@@ -24,35 +25,46 @@ log_main.log("imgPath : {}".format(imgPath), "INFO")
 
 # Defining parameter to get data from GISDA
 # URL strucute : https://disaster.gistda.or.th/api/v2/file/download?f=filename
-# Data file name : Fire/y{year}/80_Report/{fileType}/{typeData}/{typeName}_{year}{month}{day}.{fileType}
-# Image file name : Fire/y{year}/80_Report/{fileType}/{typeData}/{typename}_{year}{month}{day}.{fileType}
+# Data file name : Fire/y{year}/80_Report/{Type}/{typeData}/{typeName}_{year}{month}{day}.{fileType}
+# Image file name : Fire/y{year}/80_Report/{Type}/{typeData}/{typename}_{year}{month}{day}.{fileType}
 str_GISDAURL = "https://disaster.gistda.or.th/api/v2/file/download"
-
-dict_type_xlsx = {
-"N_Vi1_Day":"N_Vi1",
-"N_Mod_Day":"N_Mod",
-"N_Vi2_Day":"N_Vi2"
+str_Offset_params = "Fire/y"
+dict_fileStructure = {
+"excel":{
+    "Type":"Excel", 
+    "FileType":"xlsx", 
+    "TypeData":{
+        "VIIRS":"N_Vi1_Day", 
+        "MODIS":"N_Mod_Day", 
+        "NOAA20":"N_Vi2_Day"}, 
+    "TypeName":{
+        "VIIRS":"N_Vi1", 
+        "MODIS":"N_Mod", 
+        "NOAA20":"N_Vi2"}
+        },
+"image":{
+    "Type":"Map", 
+    "FileType":"jpg",
+    "TypeData":{
+        "VIIRS":"N_Vi1_Day_Thai", 
+        "MODIS":"N_Mod_Day_Thai", 
+        "NOAA20":"N_Vi2_Day_Thai"}, 
+    "TypeName":{
+        "VIIRS":"N_Vi1_Day_Thai", 
+        "MODIS":"N_Mod_Day_Thai", 
+        "NOAA20":"N_Vi2_Day_Thai"}
+        }
 }
 
-dict_type_jpg = {
-"N_Vi1_Day_Thai":"N_Vi1_Day_Thai",
-"N_Mod_Day_Thai":"N_Mod_Day_Thai",
-"N_Vi2_Day_Thai":"N_Vi2_Day_Thai"
-}
-
-dict_type_file = {
-"xlsx":"Excel",
-"jpg":"Map"
-}
+headers = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/132.0.0.0 Safari/537.36 Edg/132.0.0.0', 'referer': 'https://disaster.gistda.or.th/', 'origin': 'https://disaster.gistda.or.th'}
 
 # Defining function to get data from GISDA
 
-def getDatafromGISDA(url = None, params = None, dataPath = None, filename = None):
+def getDatafromGISDA(url = None, params = None, dataPath = None, filename = None, headers = None):
     # get data from GISDA
-    url = "https://disaster.gistda.or.th/api/v2/file/download"
-    response = requests.get(url, params = params)
-    file_Path = '{}/{}.pdf'.format(dataPath,filename)
-    log_main.log('File {} downloaded response'.format(response.content), "INFO")
+    response = requests.get(url, params = params, headers = headers)
+    file_Path = '{}/{}'.format(dataPath,filename)
+    log_main.log('File {} downloaded response {} bytes'.formatlen(filename, len(response.content)), "INFO")
     if response.status_code == 200:
         with open(file_Path, 'wb') as file:
             file.write(response.content)
@@ -64,9 +76,89 @@ def getDatafromGISDA(url = None, params = None, dataPath = None, filename = None
     log_main.log("get data {} from GISDA".format(filename), "INFO")
     log_main.log("save data to {}/{}.xlsx".format(dataPath,filename), "INFO")
 
+def startDownloadData(filename = None, thisPath = None, thisName = None):
+    params = {"f":"{}{}".format(str_Offset_params, filename)}
+    log_main.log("filename : {} - thisName : {}".format(filename, thisName), "INFO")
+    log_main.log("params : {}".format(params), "INFO")
+    try:
+        getDatafromGISDA(url = str_GISDAURL, params = params, dataPath = thisPath, filename = thisName, headers = headers)
+        with open("{}/datalist.txt".format(srcPath), "a") as file:
+            file.writelines("{} - completed\n".format(thisName))
+    except Exception as e:  
+        log_main.log("Error on get data from GISDA : {}".format(e), "ERROR")
+        with open("{}/datalist.txt".format(srcPath), "a") as file:
+            file.writelines("{} - Error : {}\n".format(thisName, e))
+    time.sleep(10)
+
 # Defining main function
 def main():
-    log_main.log("run on main : {}".format(datetime.datetime.now), "INFO")
+    # get data from GISDA
+    # get today date
+    list_args = sys.argv
+    while True:
+        thisDay = datetime.datetime.now()
+        year = thisDay.year
+        month = thisDay.month
+        day = thisDay.day
+        firstTime = False
+        todayRun = False
+        log_main.log("today : {}-{}-{}".format(year, month, day), "INFO")
+        log_main.log("args : {}".format(list_args), "INFO")
+        log_main.log("run on main : {}".format(datetime.datetime.now), "INFO")
+        if "firstTime" in list_args:
+            sys.argv = sys.argv.remove("firstTime")
+            firstTime = True
+
+        if firstTime:
+            list_Day = list()
+            startDay = datetime.datetime(year-1, month, day)
+            log_main.log("get data from : {}-{}-{}".format(year, month, day), "INFO")
+            lengthday = thisDay - startDay
+            firstTime = False
+            for day in range(0, lengthday.days):
+                list_Day.append(startDay + timedelta(days = day))
+            log_main.log("list_Day : {}".format(list_Day), "INFO")
+
+            for day in list_Day:
+                year = day.year
+                month = day.month
+                day = day.day 
+                thisPath = None  
+                thisName = None
+                for key, value in dict_fileStructure.items():
+                    if key == "excel":
+                        for keyData, valueData in value["TypeData"].items():
+                            thisPath = dataPath
+                            thisName = "{}_{}{:02d}{:02d}.{}".format(keyData, year, month, day, value["FileType"])
+                            filename = "{}/80_Report/{}/{}/{}_{}{:02d}{:02d}.{}".format(year, value["Type"], valueData, value["TypeName"][keyData], year, month, day, value["FileType"])
+                            startDownloadData(filename = filename, thisPath = thisPath, thisName = thisName)
+                    elif key == "image":
+                        for keyData, valueData in value["TypeData"].items():
+                            thisPath = imgPath
+                            thisName = "{}_{}{:02d}{:02d}.{}".format(keyData, year, month, day, value["FileType"])
+                            filename = "{}/80_Report/{}/{}/{}_{}{:02d}{:02d}.{}".format(year, value["Type"], valueData, value["TypeName"][keyData], year, month, day, value["FileType"])
+                            startDownloadData(filename = filename, thisPath = thisPath, thisName = thisName)
+            firstTime = False
+
+        elif thisDay.hour == 9 and thisDay.minute > 0 and todayRun == False:
+            thisPath = None
+            thisName = None
+            for key, value in dict_fileStructure.items():
+                if key == "excel":
+                    for keyData, valueData in value["TypeData"].items():
+                        thisPath = dataPath
+                        thisName = "{}_{}{:02d}{:02d}.{}".format(keyData, year, month, day, value["FileType"])
+                        filename = "{}/80_Report/{}/{}/{}_{}{:02d}{:02d}.{}".format(year, value["Type"], valueData, value["TypeName"][keyData], year, month, day, value["FileType"])
+                        startDownloadData(filename = filename, thisPath = thisPath, thisName = thisName)
+                elif key == "image":
+                    for keyData, valueData in value["TypeData"].items():
+                        thisPath = imgPath
+                        thisName = "{}_{}{:02d}{:02d}.{}".format(keyData, year, month, day, value["FileType"])
+                        filename = "{}/80_Report/{}/{}/{}_{}{:02d}{:02d}.{}".format(year, value["Type"], valueData, value["TypeName"][keyData], year, month, day, value["FileType"])
+                        startDownloadData(filename = filename, thisPath = thisPath, thisName = thisName)
+            todayRun = True
+        else:
+            time.sleep(1800)
 
 
 # Using the special variable 
